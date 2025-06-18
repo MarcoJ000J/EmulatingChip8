07a0a8d83639f676375ed8e28f5b09ef4a3a732c
MarcoJ000J <marcoantoniofrj2003@gmail.com>
2025-06-18 18:59:10 -0300

    trying my best, sdl configured....? how to use ? aff

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include "SDL2/SDL.h"
+#include "SDL3/SDL.h"
 
 bool quit = false;
 
 int main() {
-    
-    
+    // Initialize SDL
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* screen = SDL_CreateWindow("8Bit Emulator", 640, 320, SDL_WINDOW_OPENGL);
 
+    
+    
     return 0;
 }
